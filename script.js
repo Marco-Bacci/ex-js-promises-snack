04e691a1e900d355c21a6d1540d1f806c26485cb
Marco Bacci <marcobacci94@gmail.com>
2025-11-01 19:09:16 +0100

    Feat: snack 2 build up

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 function getPostTitle(id) {
   return new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
-      .then(resp => resp.json())
-      .then(obj => resolve(obj.title))
-      .catch(error => reject(error));
+      .then((resp) => resp.json())
+      .then((obj) => resolve(obj.title))
+      .catch((error) => reject(error));
   });
 }
 
@@ -14,3 +14,21 @@ getPostTitle(1)
   .then((title) => console.log("Titolo:", title))
   .catch((error) => console.error(error));
 
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+function lanciaDado() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let numero = Math.floor(Math.random() * 6) + 1;
+      if (numero) {
+        resolve(numero);
+      } else {
+        reject();
+      }
+    });
+  }, 3000);
+}
+
+lanciaDado()
+.then(num => console.log("il numero uscito è:" , num))
+.catch(error => console.error(error))
